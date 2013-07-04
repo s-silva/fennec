9c70d59591335ac988b7e4845bef3873e60c105a
Sandaruwan Silva <c-h@users.sf.net>
2013-07-04 21:25:27 +0530

    finalized installation.

--- installation/windows/build.bat
+++ installation/windows/build.bat
@@ -1,6 +1,6 @@
 set path=C:\Program Files (x86)\WiX Toolset v3.7\bin;%path%
 
-candle.exe SampleWixUI.wxs
-light.exe -ext WixUIExtension SampleWixUI.wixobj
+candle.exe setup.wxs
+light.exe -ext WixUIExtension setup.wixobj
 
 pause
